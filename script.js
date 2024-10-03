b59b9047a3831581d50e61cb4707deda51378b91
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-03 22:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toCase(text) {
   // write your code here
-	if (str === '') {
+	if (text === '') {
     return '-';
   }
 
   // Convert the string to lowercase and uppercase, join with '-'
-  return str.toLowerCase() + '-' + str.toUpperCase();
+  return text.toLowerCase() + '-' + str.toUpperCase();
 
 }
 
@@ -13,4 +13,5 @@ function toCase(text) {
 // DO not change the code below
 
 const text = prompt("Enter text:");
+//
 alert(toCase(text));
