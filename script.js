79a085139e012706f725ad44c660a40e6a2b4c1d
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-03 22:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toCase(text) {
   // write your code here
+	if (str === '') {
+    return '-';
+  }
+
+  // Convert the string to lowercase and uppercase, join with '-'
+  return str.toLowerCase() + '-' + str.toUpperCase();
+
 }
+
 
 // DO not change the code below
 
